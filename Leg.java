e292878d53ae1531d5c9e8be2cc5c7964b0b2fcb
Aleksander Tsõganov <aleksander.tsoganov@catapultlabs.eu>
2017-11-16 10:42:46 +0200

    initial

---
+++ Leg.java
@@ -0,0 +1,46 @@
+package com.homework;
+
+public class Leg {
+    private Stop from;
+    private Stop to;
+    private int duration;
+
+    public Leg(Stop from, Stop to, int duration) {
+        this.from = from;
+        this.to = to;
+        this.duration = duration;
+    }
+
+    public Stop getFrom() {
+        return from;
+    }
+
+    public void setFrom(Stop from) {
+        this.from = from;
+    }
+
+    public Stop getTo() {
+        return to;
+    }
+
+    public void setTo(Stop to) {
+        this.to = to;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Leg{" +
+                "from=" + from +
+                ", to=" + to +
+                ", duration=" + duration +
+                '}';
+    }
+}
---
+++ Schedule.java
@@ -0,0 +1,31 @@
+package com.homework;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class Schedule {
+
+
+    private List<LocalTime> times;
+
+
+    public Schedule(List<LocalTime> times) {
+        this.times = times;
+    }
+
+    public List<LocalTime> getTimes() {
+        return times;
+    }
+
+    public void setTimes(List<LocalTime> times) {
+        this.times = times;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Schedule{" +
+                "times=" + times +
+                '}';
+    }
+}
