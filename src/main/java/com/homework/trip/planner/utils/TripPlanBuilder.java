2982818670b18ad7208e81cd4afdcd89765cbf2a
Aleksander Tsõganov <aleksander.tsoganov@catapultlabs.eu>
2017-11-20 16:25:43 +0200

    merge

--- src/main/java/com/homework/trip/planner/utils/TripPlanBuilder.java
+++ src/main/java/com/homework/trip/planner/utils/TripPlanBuilder.java
@@ -12,7 +12,6 @@ import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TripPlanBuilder {
 
